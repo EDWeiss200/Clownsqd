96f06dd0d925edb7931e139687f5ab0e5e2a40bc
FeRRRiJane <mykhoedovs@gmail.com>
2025-07-13 17:04:55 +0700

    up: 0.0.2 models add

--- app/backend/src/models/models.py
+++ app/backend/src/models/models.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import DeclarativeBase,Mapped,mapped_column,relationship
 from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
 from sqlalchemy import Boolean, ForeignKey, Integer, String, Column,MetaData,Date
 from schemas.schemas import UserReadSchema
-
+from datetime import datetime
 intpk = Annotated[int, mapped_column(index = True,primary_key=True)]
 
 
@@ -43,3 +43,30 @@ class User(SQLAlchemyBaseUserTable[int],Base):
         )
 
 
+class Chat(Base):
+    __tablename__= "chats"
+    chat_id:Mapped[intpk] = mapped_column(
+        index=True
+    )
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey('users.id')
+    )
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey('users.id')
+    )
+    created_on: Mapped[datetime] = mapped_column()
+
+
+class Message(Base):
+    __tablename__= "messages"
+    message_id:Mapped[intpk] = mapped_column(
+        index=True
+    )
+    chat_id:Mapped[intpk] = mapped_column(
+        ForeignKey('chats.chat_id')
+    )
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey('users.id')
+    )
+    text: Mapped[str] = mapped_column()
+    created_on: Mapped[datetime] = mapped_column()
